e35546bf9ba28433d164a9b3d76538638ee95ff1
Jake Robbins <jrobbinsd@gmail.com>
2018-01-22 13:37:40 -0700

    interpratation table built

--- database/cDatabase/migrations/20180122111348_interpretation.js
+++ database/cDatabase/migrations/20180122111348_interpretation.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+  return Promise.all([
+    knex.schema.createTable('interpretation', (table) => {
+      table.increments().primary
+      table.integer('quote_id').references('quote.id')
+      table.integer('user_id').references('user.id')
+			table.text('body')
+
+
+    })
+  ])
+}
+
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('interpretation')
+  ])
 };
