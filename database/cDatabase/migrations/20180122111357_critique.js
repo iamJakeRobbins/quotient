fc03071f27f0a10bec1cfb1c6c655119caf7be69
Jake Robbins <jrobbinsd@gmail.com>
2018-01-22 13:37:56 -0700

    critiques table built

--- database/cDatabase/migrations/20180122111357_critique.js
+++ database/cDatabase/migrations/20180122111357_critique.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+  return Promise.all([
+    knex.schema.createTable('critique', (table) => {
+      table.increments().primary
+      table.integer('user_id').references('user.id')
+      table.integer('interpretation_id').references('interpretation.id')
+			table.integer('quote_id').references('quote.id')
+			table.text('body')
+
+
+    })
+  ])
+}
+
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('critique')
+  ])
 };
