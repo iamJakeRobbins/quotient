da1f8d03268a59fb0d326ffc66390d16e8063080
Jake Robbins <jrobbinsd@gmail.com>
2018-01-22 13:37:22 -0700

    quote table built

--- database/cDatabase/migrations/20180122111338_quote.js
+++ database/cDatabase/migrations/20180122111338_quote.js
@@ -1,8 +1,21 @@
 
+
 exports.up = function(knex, Promise) {
-  
-};
+  return Promise.all([
+    knex.schema.createTable('quote', (table) => {
+      table.increments().primary
+      table.text('body')
+			table.text('attribution')
+			table.text('category')
+			table.boolean('featured')
+			table.boolean('been_featured')
+    })
+  ])
+}
+
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('quote')
+  ])
 };
