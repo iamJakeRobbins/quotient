d2638e002dbdf66e1436647b5772821a40612d71
Jake Robbins <jrobbinsd@gmail.com>
2018-01-22 13:37:02 -0700

    user table built

--- database/cDatabase/migrations/20180122111320_user.js
+++ database/cDatabase/migrations/20180122111320_user.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+  return Promise.all([
+    knex.schema.createTable('user', (table) => {
+      table.increments().primary
+      table.text('username')
+      table.integer('password')
+
+
+    })
+  ])
+}
+
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('user')
+  ])
 };
