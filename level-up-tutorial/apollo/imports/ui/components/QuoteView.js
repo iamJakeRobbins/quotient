b1853b15df3ebcaab1b8b5b3225ba1b454455241
Jake Robbins <jrobbinsd@gmail.com>
2018-01-29 09:46:15 -0700

    quotes accept interpretations

--- level-up-tutorial/apollo/imports/ui/components/QuoteView.js
+++ level-up-tutorial/apollo/imports/ui/components/QuoteView.js
@@ -15,6 +15,22 @@ export default class QuoteView extends Component{
 			<FormControl className="form-control" componentClass="textarea" placeholder="Enter your Interpretation of this quote here " />
 			<Button type="submit">Submit your Interpretation</Button>
 		</FormGroup>
+		<ListGroup className='listgroup'>
+			<ListGroupItem a href="#">
+				{/* <h5>
+					Enter a meaningful bit of contemplation here, this is just a sample but if we were doing this for real I would expect you to be thinking on it for a while first.  Also, you should probably check your grammar silly. <br />
+					by: jrobbins
+				</h5> */}
+			</ListGroupItem>
+			</ListGroup>
+<ListGroup className='listgroup'>
+	<ListGroupItem a href="#">
+		<h5>
+			Education would be much more effective if its purpose was to ensure that by the time they leave school every boy and girl should know how much they do not know, and be imbued with a lifelong desire to know it. <br />
+			by: WriTerGuy123
+		</h5>
+	</ListGroupItem>
+	</ListGroup>
 		</div>
 
 		)
